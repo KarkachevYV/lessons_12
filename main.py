89f3df50f773deb700767ce13b25b867982d2015
Karkach <Karkach_Y_V@outlook.com>
2024-12-14 02:02:47 +0300

    введение в парсинг

--- main.py
+++ main.py
@@ -16,13 +16,8 @@ import requests
 #print(response.content)
 #response_json = response.json()
 #print(f"Количество репозиториев с использованием js: {response_json['total_count']}")
-#response = requests.get('https://api.example.com/data')
-#data = response.json()  # Преобразуем ответ в словарь
 
-# Предположим, что JSON-объект содержит ключ 'count'
-#ount = data.get('count')  # Извлекаем количество
-#print(count)
-img = ""
+img = "https://i.pinimg.com/236x/75/91/08/759108b7ad69db854d9a6b9977324076.jpg"
 
 response = requests.get(img)
 with open("test.jpg" "wb") as file:
