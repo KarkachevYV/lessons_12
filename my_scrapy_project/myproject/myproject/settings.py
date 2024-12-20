01f9edb5849f007a19a00c56a3657ea4ca4d23ed
Karkach <Karkach_Y_V@outlook.com>
2024-12-20 23:17:51 +0300

    Файл с настройками проекта

--- my_scrapy_project/myproject/myproject/settings.py
+++ my_scrapy_project/myproject/myproject/settings.py
@@ -12,7 +12,6 @@ BOT_NAME = "myproject"
 SPIDER_MODULES = ["myproject.spiders"]
 NEWSPIDER_MODULE = "myproject.spiders"
 
-
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
 #USER_AGENT = "myproject (+http://www.yourdomain.com)"
 
