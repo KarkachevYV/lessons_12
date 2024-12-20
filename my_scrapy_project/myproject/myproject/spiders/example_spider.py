63c7e48a0818641f374426b511e1c899dd49a896
Karkach <Karkach_Y_V@outlook.com>
2024-12-20 23:03:28 +0300

    это программа или скрипт, разработанный для автоматического обхода веб-сайтов, извлечения данных и индексации содержимого

---
+++ my_scrapy_project/myproject/myproject/spiders/example_spider.py
@@ -0,0 +1,16 @@
+import scrapy
+
+
+class ExampleSpiderSpider(scrapy.Spider):
+    name = "example_spider"
+    allowed_domains = ["https://divan.ru"]
+    start_urls = ["https://divan.ru/category/divany-i-kresla"]
+
+    def parse(self, response):
+        divans = response.css('div._Ud0k')
+        for divan in divans:
+          yield {
+             'name' : divan.css('div.lsooF span::text').get(),
+             'price' : divan.css('div.q5Uds span::text').get(),
+             'url' : divan.css('a').attrib['href']
+          }
